f76c625e441d5dbe1f483f478d8b1b12a7371081
i-diordiev <i.diordiev-2027@kpi.ua>
2021-03-22 21:26:17 +0200

    test

--- 2sem/test/test/test.cpp
+++ 2sem/test/test/test.cpp
@@ -1,181 +1,105 @@
 #include <iostream>
-#include <iomanip>
 #include <cmath>
+#include <iomanip>
 using namespace std;
-
-double find_max(double[5], double[5]);
-void outputing_matr(double[5][5]);
-void outputing_vect(double[5]);
-double* perevirka(double[5][5], double[5], double[5]);
+void outputing_matr(double[4][4]);
 int main()
 {
-	setlocale(LC_ALL, "rus");
-	double A[5][5] =
-	{
-	{5.93, 1.12, 0.95, 1.32, 0.83},
-	{0.00, 3.318, 1.941, 0.321, 0.753},
-	{0.00, 1.941, 6.728, 1.078, 1.437},
-	{0.00, 0.321, 1.078, 3.526, 1.065},
-	{0.00, 0.753, 1.437, 1.065, 5.844}
-	};
-	double b[] = { 7.24, 1.843, 2.07, 4.638, 4.987 };
+    const int m = 4;
+    double A[m][m] = { {6.48, 1.3, 0.77, 1.21}, 
+        {1.3, 3.94, 1.3, 0.16},
+        {0.77, 1.3, 5.66, 2.1},
+        {1.21, 0.16, 2.1, 5.88} };
+    double M[m][m] = { {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0} };
+    double M_dop[m][m] = { {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0} };
 
-	double x_previous[] = { 0.0,0.0,0.0,0.0,0.0 };
-	double x[5];
+    cout << "Our Matrix" << endl;
+    outputing_matr(A);
+    cout << endl;
+    for (int k = 1; k < m; k++)
+    {
+        cout << "Iteration № " << k << "\nKey element is :" << A[m - k][m - k - 1] << endl;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (i != m - k - 1)
+                {
+                    if (i == j)
+                    {
+                        M[i][j] = 1;
+                        M_dop[i][j] = 1;
+                    }
+                    else
+                    {
+                        M[i][j] = 0;
+                        M_dop[i][j] = 0;
+                    }
+                }
+                else
+                {
+                    M_dop[i][j] = A[i + 1][j];
+                    if (i == j)
+                    {
+                        M[i][j] = (double)1 / (A[i + 1][i]);
+                    }
+                    else
+                    {
+                        M[i][j] = -A[i + 1][j] / A[i + 1][i];
+                    }
+                }
 
+            }
+        }
+        double dop_A_with_M[m][m] = { {0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0} };
 
-	double C[5][5] = {
-	{0.0, 0.0, 0.0, 0.0, 0.0},
-	{0.0, 0.0, 0.0, 0.0, 0.0},
-	{0.0, 0.0, 0.0, 0.0, 0.0},
-	{0.0, 0.0, 0.0, 0.0, 0.0},
-	{0.0, 0.0, 0.0, 0.0, 0.0} };
-	double d[5] = { 0,0,0,0 };
-	double eps = 0.000001;
+        for (int t = 0; t < m; t++)
+        {
+            for (int r = 0; r < m; r++)
+            {
+                dop_A_with_M[t][r] = 0;
+                for (int s = 0; s < m; s++)
+                {
+                    dop_A_with_M[t][r] += M_dop[t][s] * A[s][r];
+                }
+            }
+        }
 
-	float max_e = 0;
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 5; j++)
-		{
-			if (A[i][j] > max_e)
-				max_e = A[i][j];
-		}
-	}
+        for (int t = 0; t < m; t++)
+        {
+            for (int r = 0; r < m; r++)
+            {
+                A[t][r] = 0;
+                for (int s = 0; s < m; s++)
+                {
+                    A[t][r] += dop_A_with_M[t][s] * M[s][r];
+                }
+            }
+        }
 
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 5; j++)
-		{
-
-			A[i][j] = A[i][j] / max_e;
-		}
-		b[i] = b[i] / max_e;
-		x[i] = b[i];
-	}
-	cout << "Матриця А" << endl;
-	outputing_matr(A);
-	cout << "Вектор вiльних членiв " << endl;
-	outputing_vect(b);
-	cout << endl;
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++)
-		{
-			if (i != j)
-			{
-				C[i][j] = -(A[i][j] / A[i][i]);
-			}
-			else C[i][j] = 0;
-		}
-		d[i] = b[i] / A[i][i];
-		x_previous[i] = d[i];
-	}
-	int counter = 0;
-	while (true) {
-		counter++;
-		for (int i = 0; i < 5; i++)
-		{
-			float s = 0;
-			for (int j = 0; j < 5; j++)
-			{
-				s = s + C[i][j] * x_previous[j];
-			}
-			x[i] = s + d[i];
-		}
-		float max = find_max(x, x_previous);
-
-		for (int i = 0; i < 5; i++)
-		{
-			x_previous[i] = x[i];
-		}
-
-
-		cout << "Iteration №" << counter << endl;
-		double* vector_nezviazky = new double[5];
-		for (int i = 0; i < 5; i++)
-			vector_nezviazky[i] = 0;
-		vector_nezviazky = perevirka(A, x, b);
-		cout << "Вектор незвязки:" << endl;
-		outputing_vect(vector_nezviazky);
-		cout << "Вектор розвязкiв  " << endl;
-		outputing_vect(x);
-		cout << endl;
-
-		if (max < eps)
-		{
-			break;
-		}
-	};
-	cout << "Iteration №" << counter << endl;
-	double* vector_nezviazky = new double[5];
-	for (int i = 0; i < 5; i++) {
-		vector_nezviazky[i] = 0;
-	}
-	vector_nezviazky = perevirka(A, x, b);
-	cout << "Вектор незвязки:" << endl;
-	outputing_vect(vector_nezviazky);
-	cout << "Вектор розвязкiв  " << endl;
-	outputing_vect(x);
-	cout << endl;
+        cout << "Matrix M^-1:" << endl;
+        outputing_matr(M_dop);
+        cout << "Matrix M:" << endl;
+        outputing_matr(M);
+        cout << "Matrix A" << endl;
+        outputing_matr(A);
+        cout << endl;
 
 
-	return 0;
+    }
+
+    return 0;
 }
 
-void outputing_matr(double matr[5][5])
+void outputing_matr(double matr[4][4])
 {
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 5; j++)
-		{
-			cout << fixed << setw(10) << setprecision(6) << matr[i][j];
-		}
-		cout << endl;
-	}
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << fixed << setw(11) << setprecision(6) << matr[i][j];
+        }
+        cout << endl;
+    }
 
 }
-
-void outputing_vect(double x[5])
-{
-	for (int i = 0; i < 5; i++)
-	{
-		cout << fixed << setw(10) << setprecision(6) << x[i];
-	}
-	cout << endl;
-}
-
-
-double find_max(double x[5], double x_pop[5])
-{
-	double max = fabs(x[0] - x_pop[0]);
-	for (int i = 1; i < 5; i++)
-	{
-		if (fabs(x[i] - x_pop[i]) > max)
-			max = fabs(x[i] - x_pop[i]);
-	}
-	return max;
-}
-
-
-double* perevirka(double A[5][5], double x[5], double b[5])
-{
-	double* dop_matr = new double[5];
-	for (int i = 0; i < 5; i++)
-		dop_matr[i] = 0;
-	for (int i = 0; i < 5; i++)
-	{
-
-		for (int j = 0; j < 5; j++)
-			dop_matr[i] += A[i][j] * x[j];
-	}
-
-	for (int i = 0; i < 5; i++)
-	{
-		dop_matr[i] = b[i] - dop_matr[i];
-
-	}
-
-	return dop_matr;
-}
-
-
--- 2sem/test/test_sharp/Program.cs
+++ 2sem/test/test_sharp/Program.cs
@@ -1,12 +1,123 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace test_sharp
+namespace srm4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = 4;
+            double a = 0.88,
+                   b = -0.02,
+                   d = 0.12;
+            double[,] matrixA = { {6.48, 1.3, 0.77, 1.21},
+        {1.3, 3.94, 1.3, 0.16},
+        {0.77, 1.3, 5.66, 2.1},
+        {1.21, 0.16, 2.1, 5.88} };
+            Console.WriteLine("A:\n");
+            Output(matrixA, n);
+            Danilovskiy(matrixA, n);
+
+        }
+        static void Output(double[,] matrix, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(String.Format("{0:f5}", matrix[i, j]) + "       ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        static double[,] MatM(double[,] A, int n, int i, int j)
+        {
+            double[,] M = new double[n, n];
+
+            for (int k = 0; k < n; k++)
+            {
+                M[k, k] = 1;
+                if (k == j)
+                    M[i - 1, k] = 1 / A[i, j];
+                else
+                    M[i - 1, k] = (-1) * A[i, k] / A[i, j];
+            }
+            return M;
+        }
+        static double[,] InvM(double[,] A, int n, int i, int j)
+        {
+            double[,] invM = new double[n, n];
+
+            for (int k = 0; k < n; k++)
+            {
+                invM[k, k] = 1;
+                invM[i - 1, k] = A[i, k];
+            }
+            return invM;
+        }
+        static double[,] Mnoj(double[,] a, double[,] b, int n)
+        {
+            double[,] c = new double[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    c[i, j] = 0;
+                    for (int k = 0; k < n; k++)
+                        c[i, j] += a[i, k] * b[k, j];
+                }
+            }
+            return c;
+        }
+        static void Danilovskiy(double[,] matrixA, int n)
+        {
+            double[,] M = new double[n, n];
+            double[,] invM = new double[n, n];
+
+            int i = n - 1, j = n - 2;
+            for (int m = 1; m < 4; m++)
+            {
+                Console.WriteLine("Iтерацiя {0},    елемент:{1:f6}", m, matrixA[i, j]);
+                Console.WriteLine("M{0}:", i);
+                M = MatM(matrixA, n, i, j);
+                Output(M, n);
+                Console.WriteLine("M^(-1){0}:", i);
+                invM = InvM(matrixA, n, i, j);
+                Output(invM, n);
+                Console.WriteLine("D{0}:", m);
+                matrixA = Mnoj(invM, matrixA, n);
+                matrixA = Mnoj(matrixA, M, n);
+                Output(matrixA, n);
+                i--; j--;
+            }
+            Console.WriteLine("P:");
+            Output(matrixA, n);
+            Polinom(matrixA, n);
+
+        }
+        static void Polinom(double[,] matrixA, int n)
+        {
+            Console.WriteLine("Характеристичне рiвняння:");
+            int i = 0;
+            for (int k = 4; k >= 0; k--)
+            {
+                if (k == 4)
+                    Console.Write("x^{0} ", k);
+                else
+                {
+                    if (matrixA[0, i] < 0)
+                        Console.Write("+ {0:f5}*x^{1} ", -1 * matrixA[0, i], k);
+                    else
+                        Console.Write("- {0:f5}*x^{1} ", matrixA[0, i], k);
+                    i++;
+                }
+            }
+            Console.WriteLine("= 0");
         }
     }
 }
