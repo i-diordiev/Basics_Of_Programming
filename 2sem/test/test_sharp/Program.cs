b97399e8cd33f869a2a21f4e9e40f669a82a7152
i-diordiev <i.diordiev-2027@kpi.ua>
2021-03-25 10:04:38 +0200

    test

--- 2sem/test/test_sharp/Program.cs
+++ 2sem/test/test_sharp/Program.cs
@@ -10,114 +10,51 @@ namespace srm4
     {
         static void Main(string[] args)
         {
-            int n = 4;
-            double a = 0.88,
-                   b = -0.02,
-                   d = 0.12;
-            double[,] matrixA = { {6.48, 1.3, 0.77, 1.21},
-        {1.3, 3.94, 1.3, 0.16},
-        {0.77, 1.3, 5.66, 2.1},
-        {1.21, 0.16, 2.1, 5.88} };
-            Console.WriteLine("A:\n");
-            Output(matrixA, n);
-            Danilovskiy(matrixA, n);
+            int[] massiv = { 2, 1, 4, 5, 3 };
+            int cnt = 0;
+            Merge_Sort(massiv, ref cnt);
+            Console.WriteLine(cnt);
 
         }
-        static void Output(double[,] matrix, int n)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    Console.Write(String.Format("{0:f5}", matrix[i, j]) + "       ");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-        }
-        static double[,] MatM(double[,] A, int n, int i, int j)
-        {
-            double[,] M = new double[n, n];
 
-            for (int k = 0; k < n; k++)
+        public static int[] Merge_Sort(int[] arr, ref int counter)
+        {
+            if (arr.Length == 1)
+                return arr;
+            int mid_point = arr.Length / 2;
+            int[] a;
+
+
+            Merge(Merge_Sort(arr.Take(mid_point).ToArray(), ref counter), Merge_Sort(arr.Skip(mid_point).ToArray(), ref counter), out a, ref counter);
+            return a;
+        }
+
+        public static void Merge(int[] mass1, int[] mass2, out int[] merged, ref int counter)
+        {
+            int a = 0, b = 0;
+            merged = new int[mass1.Length + mass2.Length];
+            for (int i = 0; i < mass1.Length + mass2.Length; i++)
             {
-                M[k, k] = 1;
-                if (k == j)
-                    M[i - 1, k] = 1 / A[i, j];
+                if (b < mass2.Length && a < mass1.Length)
+                    if (mass1[a] > mass2[b])
+                    {
+                        merged[i] = mass2[b++];
+                        counter = counter + (mass1.Length - a);
+                    }
+                    else //if int go for 
+                    {
+                        merged[i] = mass1[a++];
+                    }
                 else
-                    M[i - 1, k] = (-1) * A[i, k] / A[i, j];
-            }
-            return M;
-        }
-        static double[,] InvM(double[,] A, int n, int i, int j)
-        {
-            double[,] invM = new double[n, n];
-
-            for (int k = 0; k < n; k++)
-            {
-                invM[k, k] = 1;
-                invM[i - 1, k] = A[i, k];
-            }
-            return invM;
-        }
-        static double[,] Mnoj(double[,] a, double[,] b, int n)
-        {
-            double[,] c = new double[n, n];
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
+                    if (b < mass2.Length)
                 {
-                    c[i, j] = 0;
-                    for (int k = 0; k < n; k++)
-                        c[i, j] += a[i, k] * b[k, j];
+                    merged[i] = mass2[b++];
                 }
-            }
-            return c;
-        }
-        static void Danilovskiy(double[,] matrixA, int n)
-        {
-            double[,] M = new double[n, n];
-            double[,] invM = new double[n, n];
-
-            int i = n - 1, j = n - 2;
-            for (int m = 1; m < 4; m++)
-            {
-                Console.WriteLine("Iтерацiя {0},    елемент:{1:f6}", m, matrixA[i, j]);
-                Console.WriteLine("M{0}:", i);
-                M = MatM(matrixA, n, i, j);
-                Output(M, n);
-                Console.WriteLine("M^(-1){0}:", i);
-                invM = InvM(matrixA, n, i, j);
-                Output(invM, n);
-                Console.WriteLine("D{0}:", m);
-                matrixA = Mnoj(invM, matrixA, n);
-                matrixA = Mnoj(matrixA, M, n);
-                Output(matrixA, n);
-                i--; j--;
-            }
-            Console.WriteLine("P:");
-            Output(matrixA, n);
-            Polinom(matrixA, n);
-
-        }
-        static void Polinom(double[,] matrixA, int n)
-        {
-            Console.WriteLine("Характеристичне рiвняння:");
-            int i = 0;
-            for (int k = 4; k >= 0; k--)
-            {
-                if (k == 4)
-                    Console.Write("x^{0} ", k);
                 else
                 {
-                    if (matrixA[0, i] < 0)
-                        Console.Write("+ {0:f5}*x^{1} ", -1 * matrixA[0, i], k);
-                    else
-                        Console.Write("- {0:f5}*x^{1} ", matrixA[0, i], k);
-                    i++;
+                    merged[i] = mass1[a++];
                 }
             }
-            Console.WriteLine("= 0");
         }
     }
 }
