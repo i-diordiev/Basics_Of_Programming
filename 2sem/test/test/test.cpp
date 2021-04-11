ff0aa76a81ca60d31a230fc22ea108387a70a9bf
i-diordiev <i.diordiev-2027@kpi.ua>
2021-04-11 19:31:46 +0300

    test

--- 2sem/test/test/test.cpp
+++ 2sem/test/test/test.cpp
@@ -1,105 +1,119 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
 #include <iomanip>
 using namespace std;
-void outputing_matr(double[4][4]);
-int main()
+
+void spline_interpolation(int n, double x[5], double y[5], double p, int j)
 {
-    const int m = 4;
-    double A[m][m] = { {6.48, 1.3, 0.77, 1.21}, 
-        {1.3, 3.94, 1.3, 0.16},
-        {0.77, 1.3, 5.66, 2.1},
-        {1.21, 0.16, 2.1, 5.88} };
-    double M[m][m] = { {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0} };
-    double M_dop[m][m] = { {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0} };
-
-    cout << "Our Matrix" << endl;
-    outputing_matr(A);
-    cout << endl;
-    for (int k = 1; k < m; k++)
-    {
-        cout << "Iteration № " << k << "\nKey element is :" << A[m - k][m - k - 1] << endl;
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                if (i != m - k - 1)
-                {
-                    if (i == j)
-                    {
-                        M[i][j] = 1;
-                        M_dop[i][j] = 1;
-                    }
-                    else
-                    {
-                        M[i][j] = 0;
-                        M_dop[i][j] = 0;
-                    }
-                }
-                else
-                {
-                    M_dop[i][j] = A[i + 1][j];
-                    if (i == j)
-                    {
-                        M[i][j] = (double)1 / (A[i + 1][i]);
-                    }
-                    else
-                    {
-                        M[i][j] = -A[i + 1][j] / A[i + 1][i];
-                    }
-                }
-
-            }
-        }
-        double dop_A_with_M[m][m] = { {0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0} };
-
-        for (int t = 0; t < m; t++)
-        {
-            for (int r = 0; r < m; r++)
-            {
-                dop_A_with_M[t][r] = 0;
-                for (int s = 0; s < m; s++)
-                {
-                    dop_A_with_M[t][r] += M_dop[t][s] * A[s][r];
-                }
-            }
-        }
-
-        for (int t = 0; t < m; t++)
-        {
-            for (int r = 0; r < m; r++)
-            {
-                A[t][r] = 0;
-                for (int s = 0; s < m; s++)
-                {
-                    A[t][r] += dop_A_with_M[t][s] * M[s][r];
-                }
-            }
-        }
-
-        cout << "Matrix M^-1:" << endl;
-        outputing_matr(M_dop);
-        cout << "Matrix M:" << endl;
-        outputing_matr(M);
-        cout << "Matrix A" << endl;
-        outputing_matr(A);
-        cout << endl;
-
-
-    }
-
-    return 0;
+	double a = 0, b = 0, c = 0, d = 0;
+	double h[5] = { 0,0,0,0,0 };
+	double s[5] = { 0,0,0,0,0 };
+	double matr[5][5] = {
+		{0,0,0,0,0},
+		{0,0,0,0,0},
+		{0,0,0,0,0},
+		{0,0,0,0,0},
+		{0,0,0,0,0}
+	};
+	for (int i = 0; i < n; i++)
+	{
+		s[i] = 0;
+	}
+	for (int i = 1; i <= n - 1; i++)
+	{
+		h[i - 1] = x[i] - x[i - 1];
+	}
+	for (int i = 1; i < n - 1; i++)
+	{
+		matr[i][i] = 2 * (h[i - 1] + h[i]);
+		if (i != 1)
+		{
+			matr[i][i - 1] = h[i - 1];
+			matr[i - 1][i] = h[i - 1];
+		}
+		matr[i][n - 1] = 6 * ((y[i + 1] - y[i]) / (x[i + 1] - x[i]) - (y[i] - y[i - 1]) / (x[i] - x[i - 1]));
+	}
+	double sum, k;
+	for (int i = 1; i < n - 2; i++)
+	{
+		k = (matr[i + 1][i] / matr[i][i]);
+		for (int j = 1; j <= n - 1; j++)
+			matr[i + 1][j] -= k * matr[i][j];
+	}
+	for (int i = n - 2; i > 0; i--)
+	{
+		sum = 0;
+		for (int j = i; j <= n - 2; j++)
+			sum += matr[i][j] * s[j];
+		s[i] = (matr[i][n - 1] - sum) / matr[i][i];
+	}
+	for (int i = 0; i < n - 1; i++)
+		if ((x[i] <= p) && (p <= x[i + 1]))
+		{
+			a = (s[i + 1] - s[i]) / (6 * h[i]);
+			b = s[i] / 2;
+			c = (y[i + 1] - y[i]) / h[i] - (2 * h[i] * s[i] + s[i + 1] * h[i]) / 6;
+			d = y[i] / 1;
+			sum = a * pow((p - x[i]), 3) + b * pow((p - x[i]), 2) + c * (p - x[i]) + d;
+		}
+	//cout << ":" << sum << endl << endl;
+	cout << "Коефiцiєнти сплайна для промiжку № " << j << ": " << a << " " << " " << b << " " << c << " " << d;
 }
 
-void outputing_matr(double matr[4][4])
+
+double Newton(double x1, double x[5], double y[5])
 {
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            cout << fixed << setw(11) << setprecision(6) << matr[i][j];
-        }
-        cout << endl;
-    }
+	double value = y[0], F, m;
+	for (int i = 1; i < 5; i++)
+	{
+		F = 0;
+		for (int j = 0; j <= i; j++)
+		{
+			m = 1;
+
+			for (int q = 0; q <= i; q++)
+			{
+				if (q != j)
+				{
+					m *= (x[j] - x[q]);
+				}
+			}
+
+			F += y[j] / m;
+		}
+
+		for (int k = 0; k < i; k++)
+			F *= (x1 - x[k]);
+		value += F;
+	}
+	return value;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	double x[5] = { -4, -2, 0, 2, 4 };
+	double y[5] = { -2.01, -1.958, 0, 1.958, 2.01 };
+
+	for (double i = x[0]; i <= x[4] + 0.1; i += 0.4)
+	{
+		cout << "Значення полiнома Ньютона в точцi " << fixed << setprecision(2) << i << ": ";
+		cout << Newton(i, x, y) << endl;
+
+	}
+	for (int i = x[0]; i <= x[4] + 0.1; i += 2)
+	{
+		double err = fabs(Newton(i, x, y) - sin(i) * pow(2 * i, double(1) / 3));
+		cout << "Похибка для х=" << fixed << setprecision(5) << i << "  " << err << endl;
+	}
+	int j = 1;
+	for (int i = x[0]; i < x[4]; i += 2)
+	{
+
+		spline_interpolation(5, x, y, i, j);
+		j++;
+		cout << endl;
+	}
 
 }
--- 2sem/test/test_sharp/Program.cs
+++ 2sem/test/test_sharp/Program.cs
@@ -1,60 +1,127 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace srm4
+namespace srm5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] massiv = { 2, 1, 4, 5, 3 };
-            int cnt = 0;
-            Merge_Sort(massiv, ref cnt);
-            Console.WriteLine(cnt);
+            int n = 5;
+            double[] x = { -4, -2, 0, 2, 4 };
+            double[] y = { -2.01, -1.958, 0, 1.958, 2.01 };
+            
 
+            Console.WriteLine("\nIнтерполяцiйний полiном Ньютона");
+            Newton(x, y, n);
+
+            Console.WriteLine("\nIнтерполяцiя кубiчними сплайнами ");
+            for (int i = 0; i < n; i++)
+                Spline(x, y, n, x[i]);
         }
 
-        public static int[] Merge_Sort(int[] arr, ref int counter)
+        static void Newton(double[] x, double[] y, int n)
         {
-            if (arr.Length == 1)
-                return arr;
-            int mid_point = arr.Length / 2;
-            int[] a;
+            double[] del1 = new double[n - 1];
+            double[] del2 = new double[n - 2];
+            double[] del3 = new double[n - 3];
+            double[] del4 = new double[n - 4];
 
-
-            Merge(Merge_Sort(arr.Take(mid_point).ToArray(), ref counter), Merge_Sort(arr.Skip(mid_point).ToArray(), ref counter), out a, ref counter);
-            return a;
-        }
-
-        public static void Merge(int[] mass1, int[] mass2, out int[] merged, ref int counter)
-        {
-            int a = 0, b = 0;
-            merged = new int[mass1.Length + mass2.Length];
-            for (int i = 0; i < mass1.Length + mass2.Length; i++)
+            for (int k = 0; k < n - 1; k++)
             {
-                if (b < mass2.Length && a < mass1.Length)
-                    if (mass1[a] > mass2[b])
-                    {
-                        merged[i] = mass2[b++];
-                        counter = counter + (mass1.Length - a);
-                    }
-                    else //if int go for 
-                    {
-                        merged[i] = mass1[a++];
-                    }
-                else
-                    if (b < mass2.Length)
+                del1[k] = (y[k] - y[k + 1]) / (x[k] - x[k + 1]);
+            }
+            for (int j = 0; j < n - 2; j++)
+            {
+                del2[j] = (del1[j] - del1[j + 1]) / (x[j] - x[j + 2]);
+            }
+            for (int i = 0; i < n - 3; i++)
+            {
+                del3[i] = (del2[i] - del2[i + 1]) / (x[i] - x[i + 3]);
+            }
+            for (int i = 0; i < n - 4; i++)
+            {
+                del4[i] = (del3[i] - del3[i + 1]) / (x[i] - x[i + 4]);
+            }
+            Console.WriteLine($"{y[0]:f5}+{del1[0]:f5}*(x-{x[0]}){del2[0]:f5}*(x-{x[0]})*(x-{x[1]})+{del3[0]:f5}*(x-{x[0]})*(x-{x[1]})*(x-{x[2]})+{del4[0]:f5}*(x-{x[0]})*(x-{x[1]})*(x-{x[2]})*(x-{x[3]})");
+
+        }
+
+        static void NewTon(double[] x, double[] y, int n, double X)
+        {
+            double[] Xn = y;
+            double[] delta = new double[n];
+
+            delta[0] = y[0];
+        }
+
+        static void Spline(double[] X, double[] Y, int n, double x0)
+        {
+            double[] a = new double[n - 1];
+            double[] b = new double[n - 1];
+            double[] d = new double[n - 1];
+            double[] h = new double[n - 1];
+
+            double[,] A = new double[n - 1, n];
+
+            double[] by = new double[n];
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                a[i] = Y[i];
+                h[i] = X[i + 1] - X[i];
+            }
+            A[0, 0] = 1;
+            A[n - 2, n - 2] = 1;
+            for (int i = 1; i < n - 2; i++)
+            {
+                A[i, i - 1] = h[i - 1];
+                A[i, i] = 2 * (h[i - 1] + h[i]);
+                A[i, i + 1] = h[i];
+                by[i] = 3 * (((Y[i + 1] - Y[i]) / h[i]) - ((Y[i] - Y[i - 1]) / h[i - 1]));
+            }
+            double[] c = Progon(A, by, n - 1);
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (i != n - 2)
                 {
-                    merged[i] = mass2[b++];
+                    d[i] = (c[i + 1] - c[i]) / (3 * h[i]);
+                    b[i] = ((Y[i + 1] - Y[i]) / h[i]) - h[i] * (c[i + 1] + 2 * c[i]) / 3;
                 }
                 else
                 {
-                    merged[i] = mass1[a++];
+                    d[i] = (-1) * (c[i] / (3 * h[i]));
+                    b[i] = ((Y[i] - Y[i - 1]) / h[i]) - ((2 * h[i] * c[i]) / 3);
+
                 }
             }
+            d[n - 2] = -c[n - 2] / (3 * h[n - 2]);
+            b[n - 2] = ((Y[n - 1] - Y[n - 2]) / h[n - 2]) - 2 * h[n - 2] * c[n - 2] / 3;
+
+        }
+
+        static double[] Progon(double[,] A, double[] b, int n)
+        {
+            double[] K = new double[n];
+            int n1 = n - 1;
+
+            double y = A[0, 0];
+            double[] a = new double[n];
+            double[] b1 = new double[n];
+            a[0] = -A[0, 1] / y;
+            b1[0] = b[0] / y;
+            for (int i = 1; i < n1; i++)
+            {
+                y = A[i, i] + A[i, i - 1] * a[i - 1];
+                a[i] = -A[i, i + 1] / y;
+                b1[i] = (b[i] - A[i, i - 1] * b1[i - 1]) / y;
+            }
+            K[n1] = b1[n1];
+            for (int i = n1 - 1; i >= 0; i--)
+            {
+                K[i] = a[i] * K[i + 1] + b1[i];
+            }
+            return K;
         }
     }
 }
